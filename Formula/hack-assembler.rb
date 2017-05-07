213aafff7b41d3c55db1c08ef8824e7d0469a28f
Eason Chen <easoncxz@gmail.com>
2017-05-07 16:23:31 +0800

    Add formula: hack-assembler

---
+++ Formula/hack-assembler.rb
@@ -0,0 +1,17 @@
+
+class HackAssembler < Formula
+    desc = "An assembler"
+    homepage = "https://google.com"
+    url = "http://localhost:8080/hack-aseembler.tar.gz"
+    sha256 = "f2de5f9e7c7767647ca25ad0a140dfd7929cf0fedf1aba18f016afa9b7e0995c"
+
+    depends_on "haskell-stack" => :build
+
+    def install
+        system "stack", "install"
+    end
+
+    test do
+        system "echo" "'D=D+1'" "|" "hack-assembler-exe"
+    end
+end
