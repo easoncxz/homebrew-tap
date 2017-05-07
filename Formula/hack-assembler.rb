ec4a749fd813425211164aa98fb4cd51d2d3614c
Eason Chen <easoncxz@gmail.com>
2017-05-07 16:29:38 +0800

    Use temporary Bintray URL for source tarball

--- Formula/hack-assembler.rb
+++ Formula/hack-assembler.rb
@@ -2,7 +2,7 @@
 class HackAssembler < Formula
     desc = "An assembler"
     homepage = "https://google.com"
-    url = "http://localhost:8080/hack-aseembler.tar.gz"
+    url = "https://dl.bintray.com/easoncxz/hello-bintray/hack-assembler.tar.gz"
     sha256 = "f2de5f9e7c7767647ca25ad0a140dfd7929cf0fedf1aba18f016afa9b7e0995c"
 
     depends_on "haskell-stack" => :build
