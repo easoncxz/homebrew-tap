056e2a8fc06d95aa99dd616342281addd3a09cbb
Eason Chen <easoncxz@gmail.com>
2017-05-20 20:58:47 +0800

    TravisCI/hack-assembler: Build #local-build (commit local-commit)

--- Formula/hack-assembler.rb
+++ Formula/hack-assembler.rb
@@ -2,7 +2,7 @@ class HackAssembler < Formula
   desc("A toy assembler for the Hack machine language")
   homepage("https://github.com/easoncxz/hack-assembler")
   url("https://github.com/easoncxz/hack-assembler/archive/v0.1.1.2.tar.gz")
-  sha256("d30bd0808a77667fe11bccd82e401a452505d0e7346586a5537f8c57d43e5432  v0.1.1.2.tar.gz")
+  sha256("d30bd0808a77667fe11bccd82e401a452505d0e7346586a5537f8c57d43e5432")
   depends_on("haskell-stack" => :build)
   def install
     system("stack", "setup")
