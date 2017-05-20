44a69960ed412cc419196f82a2c94b78b38c2e03
Eason Chen <easoncxz@gmail.com>
2017-05-20 18:17:04 +0800

    Fix Formula syntax errors

--- Formula/hack-assembler.rb
+++ Formula/hack-assembler.rb
@@ -1,17 +1,20 @@
 
 class HackAssembler < Formula
-    desc = "An assembler"
-    homepage = "https://google.com"
-    url = "https://dl.bintray.com/easoncxz/hello-bintray/hack-assembler.tar.gz"
-    sha256 = "f2de5f9e7c7767647ca25ad0a140dfd7929cf0fedf1aba18f016afa9b7e0995c"
+    desc "A toy assembler for the Hack machine language"
+    homepage "https://github.com/easoncxz/hack-assembler"
+    url "https://github.com/easoncxz/hack-assembler/archive/v0.1.1.2.tar.gz"
+    sha256 "d30bd0808a77667fe11bccd82e401a452505d0e7346586a5537f8c57d43e5432"
 
     depends_on "haskell-stack" => :build
 
     def install
-        system "stack", "install"
+        system "stack", "setup"
+        system "stack", "build"
+        prefix = `stack path --dist-dir`.chomp
+        bin.install "#{prefix}/build/hack-assembler/hack-assembler"
     end
 
     test do
-        system "echo" "'D=D+1'" "|" "hack-assembler-exe"
+        system "echo", "D=D+1", "|", "hack-assembler-exe"
     end
 end
