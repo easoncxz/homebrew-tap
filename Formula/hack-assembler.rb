f2c6b8af2b0e9e88507e58827d8f3e537444c154
Eason Chen <easoncxz@gmail.com>
2017-05-22 16:27:43 +0800

    Try local bottle

--- Formula/hack-assembler.rb
+++ Formula/hack-assembler.rb
@@ -4,6 +4,10 @@ class HackAssembler < Formula
   url("https://github.com/easoncxz/hack-assembler/archive/v0.1.1.2.tar.gz")
   sha256("d30bd0808a77667fe11bccd82e401a452505d0e7346586a5537f8c57d43e5432")
   depends_on("haskell-stack" => :build)
+  bottle do
+    root_url "http://localhost:8081"
+    sha256 "2fc69c2569a6e76f0c4dbffaecd63cec294c4a9ec253c91c6bd3feddbb32027c" => :yosemite
+  end
   def install
     system("stack", "setup")
     system("stack", "build")
