6524a8ff58cc1141935f952818e9b414f6773639
Eason Chen <easoncxz@gmail.com>
2017-05-24 17:12:25 +0800

    Point bottlr-url to Bintray!

--- Formula/hack-assembler.rb
+++ Formula/hack-assembler.rb
@@ -5,7 +5,9 @@ class HackAssembler < Formula
   sha256("46823a63bf32b26f09764a70babe59190461f84f8f04be796465d0756698e2a3")
   depends_on("haskell-stack" => :build)
   bottle do
-    root_url("http://localhost:8081")
+    cellar :any_skip_relocation
+    rebuild 1
+    root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
     sha256("4be7efbbfb251b46a860080b753bd280b36726e1d8b7815893e35354ae33f730" => :yosemite)
   end
   def install
