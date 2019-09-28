73a02f8f8c215174a7b10572ba233074d987e4c8
Eason Chen <easoncxz@gmail.com>
2019-09-28 19:38:12 +1200

    Add bottles for hack-assembler@0.1.1.27

--- Formula/hack-assembler.rb
+++ Formula/hack-assembler.rb
@@ -1,13 +1,13 @@
 class HackAssembler < Formula
   desc("A toy assembler for the Hack machine language")
   homepage("https://github.com/easoncxz/hack-assembler")
-  url("https://github.com/easoncxz/hack-assembler/archive/v0.1.1.26.tar.gz")
-  sha256("6db06492498052a128d0335e3ee2a0617967859fa7995230e88b4d6ae690c515")
+  url("https://github.com/easoncxz/hack-assembler/archive/v0.1.1.27.tar.gz")
+  sha256("c54b4150401456a5272ea7e6cbb32992ac1fcfefa7c1d0fc946806ae80cfa0cc")
   depends_on("haskell-stack" => :build)
   bottle do
     cellar(:any_skip_relocation)
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
-    sha256("8a8701f364b56d6c7ecd11d108a15037cc46d70e05a8f1d665a2fd79a32b7d9b" => :high_sierra)
+    sha256("026cdb419cd0fe2b08a628cfba2b6c1f7e23cf3fe6e833006a373817746eea82" => :high_sierra)
   end
   def install
     system("stack", "setup", "--verbose")
