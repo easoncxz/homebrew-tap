60ff7c29e5e2050bec79e306afb71f5d151d0ed6
Eason Chen <easoncxz@gmail.com>
2020-04-11 18:43:15 +1200

    Add bottles for hello@0.0.14

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -7,6 +7,7 @@ class Hello < Formula
   bottle do
     cellar(:any_skip_relocation)
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
+    sha256("e35d573feb3a1bc81208f00f30b2d98a9cd4420ce69d7fbf1aff076118c8c96e" => :catalina)
   end
   def install
     system("./build.sh")
