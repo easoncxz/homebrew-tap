2ed4f27694dcaa62ad6c590f6cb5ce91ef451e3b
Eason Chen <easoncxz@gmail.com>
2020-04-11 16:12:44 +1200

    Add bottles for hello@0.0.8

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,13 +1,13 @@
-
 class Hello < Formula
   desc("A trivial program")
   homepage("https://github.com/easoncxz/hello")
-  url("https://github.com/easoncxz/hello/archive/v0.0.1.tar.gz")
-  sha256("4904c9e8f79d529841cdd5eb0c17e0faa609f1be414dffb793929c2c83bc3a54")
+  url("https://github.com/easoncxz/hello/archive/v0.0.8.tar.gz")
+  sha256("acb8c2132458e6d3436d5539af54a518eeea74308d80d985cc92d4b5c6e8b31c")
   depends_on("gcc@9" => :build)
   bottle do
     cellar(:any_skip_relocation)
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
+    sha256("92fcf5a967464357c72d7e7d854533d30635e29cd1765a5fc5eaed35e37a032d" => :high_sierra)
   end
   def install
     system("./build.sh")
