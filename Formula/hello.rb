ee2f7603776f233def261aaa2e53c57fa2f04bde
runner <runner@Mac-1451.local>
2020-04-11 05:09:45 +0000

    Add bottles for hello@0.0.10

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,13 +1,12 @@
 class Hello < Formula
   desc("A trivial program")
   homepage("https://github.com/easoncxz/hello")
-  url("https://github.com/easoncxz/hello/archive/v0.0.8.tar.gz")
-  sha256("acb8c2132458e6d3436d5539af54a518eeea74308d80d985cc92d4b5c6e8b31c")
+  url("https://github.com/easoncxz/hello/archive/v0.0.10.tar.gz")
+  sha256("e6bd908bf1072242a12ff4ac4ecfb0d0c1e8ad95436b049d678db27f085b65e6")
   depends_on("gcc@9" => :build)
   bottle do
     cellar(:any_skip_relocation)
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
-    sha256("92fcf5a967464357c72d7e7d854533d30635e29cd1765a5fc5eaed35e37a032d" => :high_sierra)
   end
   def install
     system("./build.sh")
