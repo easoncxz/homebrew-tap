eca13cba7bd31d707d81758c29802a56947f3cff
Eason Chen <easoncxz@gmail.com>
2020-04-20 04:08:49 +1200

    Add bottles for hello-homebrew-packaging@0.0.2

--- Formula/hello-homebrew-packaging.rb
+++ Formula/hello-homebrew-packaging.rb
@@ -1,13 +1,13 @@
 class HelloHomebrewPackaging < Formula
   desc("A program to demonstrate Homebrew packaging")
   homepage("https://github.com/easoncxz/hello-homebrew-packaging")
-  url("https://github.com/easoncxz/hello-homebrew-packaging/archive/v0.0.1.tar.gz")
-  sha256("520c20c76b3530a3cc01c565c28d70bb54ab45cb472b5e8a1ad88d6d6e83f1c0")
+  url("https://github.com/easoncxz/hello-homebrew-packaging/archive/v0.0.2.tar.gz")
+  sha256("eecb34aa22b796e971e9ae26203ade9c35494bb422c6bdc7eff531ee84484596")
   depends_on("gcc@9" => :build)
   bottle do
-    root_url "https://dl.bintray.com/easoncxz/homebrew-bottles"
-    cellar :any_skip_relocation
-    sha256 "82266ac55cad50506dcb56c93e51e9d379a546e988f0e4263a650976a04da475" => :high_sierra
+    root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
+    cellar(:any_skip_relocation)
+    sha256("04716b943dba7ec68be2ca28fe11d3bcf3f86332022b77a5e53df64e4b2bfd56" => :high_sierra)
   end
   def install
     system("./build.sh")
