8ca0f57f2801f88ba9c4d71d485f87ef25acf337
runner <runner@Mac-1502.local>
2020-04-20 12:23:26 +0000

    Add bottles for hello-homebrew-packaging@0.0.3

--- Formula/hello-homebrew-packaging.rb
+++ Formula/hello-homebrew-packaging.rb
@@ -2,7 +2,7 @@ class HelloHomebrewPackaging < Formula
   desc("A program to demonstrate Homebrew packaging")
   homepage("https://github.com/easoncxz/hello-homebrew-packaging")
   url("https://github.com/easoncxz/hello-homebrew-packaging/archive/v0.0.3.tar.gz")
-  sha256("11afae7d62fc8b6ffd0580cb90c66f01f64d31de6ae03289a49eb1025b561cd8")
+  sha256("ba80ac7b239946c6d27c7632d5376a65ff08f25d6ec52aafe47f860fa9885e17")
   depends_on("gcc@9" => :build)
   bottle do
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
