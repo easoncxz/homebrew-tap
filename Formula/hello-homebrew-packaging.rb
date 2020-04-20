92f7d94ae32aee66c70ad16e21c9a77219fb3556
Travis CI <travis@Traviss-Mac-6.local>
2020-04-20 12:34:25 +0000

    Add bottles for hello-homebrew-packaging@0.0.3

--- Formula/hello-homebrew-packaging.rb
+++ Formula/hello-homebrew-packaging.rb
@@ -8,6 +8,7 @@ class HelloHomebrewPackaging < Formula
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
     cellar(:any_skip_relocation)
     sha256("e92cc9ac8db371e20e21c99b1b14c8f463f739d5f6bb5a7893766840bddec90a" => :mojave)
+    sha256("257aa2e4317addf83260b970e13eef4426f2792acfab945c52ccb7658eec3b2d" => :high_sierra)
     sha256("040d71d9874b038d6bedfd9c490b32dbc93f41013cc505a270a6e22f735e1e91" => :catalina)
   end
   def install
