a4539f5cbe00288bd63275b6af2659030cb28178
Travis CI <travis@Traviss-Mac.local>
2020-05-10 13:40:58 +0000

    Add bottles for weibo-export@0.1.0.2

--- Formula/weibo-export.rb
+++ Formula/weibo-export.rb
@@ -1,11 +1,11 @@
 class WeiboExport < Formula
   desc("Semi-automatic CLI tool to download mblogs from weibo.com")
   homepage("https://github.com/easoncxz/weibo-export")
-  url("https://github.com/easoncxz/weibo-export/archive/v0.1.0.0.tar.gz")
-  sha256("0290e779938ad6d4d7c96231b9ddb370ec860130a6dc42be2a01eebb9c730bfe")
+  url("https://github.com/easoncxz/weibo-export/archive/v0.1.0.2.tar.gz")
+  sha256("4b0938c442bfbde3d34037ace1024bf235f4bdf30f64319c1413d0b4cf681b07")
   depends_on("haskell-stack" => :build)
   bottle do
-    root_url "https://dl.bintray.com/easoncxz/homebrew-bottles"
+    root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
   end
   def install
     system("stack", "setup", "--verbose")
