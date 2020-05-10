7885b0991bd8a131d3c0709b80aaa840702e1fb1
Eason Chen <easoncxz@gmail.com>
2020-05-10 23:46:14 +1200

    Fix weibo-export formula: stack needs to exist as a dep...

--- Formula/weibo-export.rb
+++ Formula/weibo-export.rb
@@ -3,11 +3,11 @@ class WeiboExport < Formula
   homepage("https://github.com/easoncxz/weibo-export")
   url("https://github.com/easoncxz/weibo-export/archive/v0.1.0.0.tar.gz")
   sha256("0290e779938ad6d4d7c96231b9ddb370ec860130a6dc42be2a01eebb9c730bfe")
+  depends_on("haskell-stack" => :build)
   bottle do
     root_url "https://dl.bintray.com/easoncxz/homebrew-bottles"
   end
   def install
-    system("./automation/install-stack.sh")
     system("stack", "setup", "--verbose")
     system("stack", "build")
     prefix = `#{"stack path --dist-dir"}`.chomp
