81801b9be740fe08f7404791f88386388ba31f5c
Eason Chen <easoncxz@gmail.com>
2020-05-17 22:51:17 +1200

    Add bottles for weibo-export@0.1.0.2

--- Formula/weibo-export.rb
+++ Formula/weibo-export.rb
@@ -7,6 +7,9 @@ class WeiboExport < Formula
   bottle do
     cellar(:any_skip_relocation)
     root_url("https://dl.bintray.com/easoncxz/homebrew-bottles")
+    sha256("d4346671c142efe9c010d73094ee3293c0cb3d5261e655b576cf58d07c634c45" => :mojave)
+    sha256("d10b51d80cd6e371cc825fd9239ab96faa54ff0b0242928184b4dfd317e673aa" => :high_sierra)
+    sha256("d7b4f9a816290971eeef8bc172fbcb0eb00ab9a40a9d5b661401bd9383ee16a6" => :catalina)
   end
   def install
     system("stack", "upgrade", "--force-download")
